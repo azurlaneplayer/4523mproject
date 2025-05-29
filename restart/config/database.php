a3f372565ab2004c4607bb13112fd017ccce07dc
Azurlaneplayer <65599885+azurlaneplayer@users.noreply.github.com>
2025-05-29 17:22:32 +0800

    Update database.php

--- restart/config/database.php
+++ restart/config/database.php
@@ -1,8 +1,12 @@
 <?php
+    
+    define('DB_HOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
     define('DB_NAME','projectdb');
-    $conn = new mysqli(DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD,DB_NAME);
 
-    if(){
+    if($conn ->connect_error ){
         die("Connection failed: " . $conn->connect_error);
     }
 
